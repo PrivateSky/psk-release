af8f0ab390cac2685d0fa1ece553179b84ffe0db
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-03-27 11:42:42 +0200

    travis-ci integration

--- scripts/testrunner.js
+++ scripts/testrunner.js
@@ -6,19 +6,24 @@ require(path.join(__dirname, "../psknode/bundles/pskruntime"));
 let config = {testDirs: [process.cwd()]};
 const {testRunner, assert} = require("double-check");
 assert.begin(undefined, undefined, 10000000);
-//assert.end(undefined, 1, true);
 
 testRunner.start(config, callback);
 
 function callback(error, result) {
-    if(error) {
+    let exitCode = 0;
+    if (error) {
         console.error(error);
+        exitCode = 1;
     } else {
-        if(!result){
+        if (!result) {
             console.log("Report and results are above, please check console!");
-        }else{
+        } else {
             console.log("Finished!");
+            if (result.failed > 0) {
+                console.log("Setting exit code 1 because we have failed tests.");
+                exitCode = 1;
+            }
         }
-        process.exit(0);
     }
+    process.exit(exitCode);
 }
